9cb28b83e80e640bb2a81bd066931a4ffa0425f1
SacredGhost <llsacredghostll@gmail.com>
2023-11-21 16:23:44 -0700

    updated universal client to include racing rel

--- universal_client.py
+++ universal_client.py
@@ -1,12 +1,13 @@
 import subprocess
+import os
 
 def run_script(script_name):
     try:
-        subprocess.run(["python", script_name + ".py"], check=True)
+        subprocess.run(["python", script_name], check=True)
     except FileNotFoundError:
-        print(f"Script '{script_name}.py' not found.")
+        print(f"Script '{script_name}' not found.")
     except subprocess.CalledProcessError:
-        print(f"Error running script '{script_name}.py', please report the error")
+        print(f"Error running script '{script_name}', please report the error")
 
 def main():
     while True:
@@ -14,17 +15,21 @@ def main():
         print("1. AutoSplitter")
         print("2. Hide and Seek")
         print("3. Shared Stats")
-        print("4. Exit")
+        print("4. Racing REL")
+        print("5. Exit")
 
         choice = input("Enter your choice: ")
+        # choice = "1" # debug
 
         if choice == "1":
-            run_script("autosplitter")
+            run_script("autosplitter.py")
         elif choice == "2":
-            run_script("hide_client")
+            run_script("hide_client.py")
         elif choice == "3":
-            run_script("ss_client")
+            run_script("ss_client.py")
         elif choice == "4":
+            run_script(os.path.join("SPMRacingREL", "visualClient.py"))
+        elif choice == "5":
             break
         else:
             print("Invalid Choice")
